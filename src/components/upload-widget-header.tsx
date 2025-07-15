23747036756097917e09110b905258345dca5f9c
Euller Braz <euller.braz99@gmail.com>
2025-07-15 08:58:27 -0300

    feat: Add collapsible behavior on tap upload widget header

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,3 +1,4 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
 import { Minimize2 } from 'lucide-react';
 import { Button } from './ui/button';
 
@@ -5,11 +6,12 @@ export function UploadWidgetHeader() {
   return (
     <div className='w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between'>
       <span className='text-sm font-medium'> Upload files</span>
-      <button>
+
+      <Collapsible.Trigger asChild>
         <Button size='icon' className='-mr-2'>
           <Minimize2 strokeWidth={1.5} className='size-4' />
         </Button>
-      </button>
+      </Collapsible.Trigger>
     </div>
   );
 }

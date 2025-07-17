ba3c8cbb12f2ae6def830f7baabed244e520a5bd
Euller Braz <euller.braz99@gmail.com>
2025-07-17 20:36:44 -0300

    feat: Create compress image feature

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,6 +4,7 @@ import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 import { useShallow } from 'zustand/shallow';
 import { uploadFileToStorage } from '../http/upload-file-to-storage';
+import { compressImage } from '../utils/compress-image';
 
 export type Upload = {
   name: string;
@@ -12,6 +13,8 @@ export type Upload = {
   status: 'progress' | 'success' | 'error' | 'canceled';
   originalSizeInBytes: number;
   uploadSizeInBytes: number;
+  compressedSizeInBytes?: number;
+  remoteUrl?: string;
 };
 
 type UploadState = {
@@ -44,9 +47,18 @@ export const useUploads = create<UploadState & UploadActions>()(
       if (!upload) return;
 
       try {
-        await uploadFileToStorage(
+        const compressedFile = await compressImage({
+          file: upload.file,
+          maxWidth: 1000,
+          maxHeight: 1000,
+          quality: 0.8,
+        });
+
+        updateUpload(uploadId, { compressedSizeInBytes: compressedFile.size });
+
+        const { url } = await uploadFileToStorage(
           {
-            file: upload.file,
+            file: compressedFile,
             onProgress(sizeInBytes) {
               updateUpload(uploadId, {
                 uploadSizeInBytes: sizeInBytes,
@@ -56,7 +68,7 @@ export const useUploads = create<UploadState & UploadActions>()(
           { signal: upload.abortController.signal }
         );
 
-        updateUpload(uploadId, { status: 'success' });
+        updateUpload(uploadId, { status: 'success', remoteUrl: url });
       } catch (error) {
         if (error instanceof CanceledError) {
           return updateUpload(uploadId, { status: 'canceled' });
@@ -115,8 +127,12 @@ export const usePendingUploads = () => {
 
       const { total, uploaded } = uploads.reduce(
         (acc, upload) => {
-          acc.total += upload.originalSizeInBytes;
-          acc.uploaded += upload.uploadSizeInBytes;
+          if (upload.compressedSizeInBytes) {
+            acc.uploaded += upload.uploadSizeInBytes;
+          }
+
+          acc.total +=
+            upload.compressedSizeInBytes || upload.originalSizeInBytes;
 
           return acc;
         },
